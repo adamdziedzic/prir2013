b597adae18bf83b8c25693bb41774de455b03291
adrian <adrian@adrian-laptop>
2013-05-24 18:55:16 +0200

    * add generate genotype method

--- genethicPRIR/src/genethicprir/Population.java
+++ genethicPRIR/src/genethicprir/Population.java
@@ -5,6 +5,7 @@
 package genethicprir;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
@@ -25,8 +26,50 @@ public class Population {
 	}
         
         private int[] generateGenotype(){   
-            //TODO
-            return null;
+            int [] vec = new int[21];
+            Random rand = new Random();
+            
+            //range 8-18
+            int lengthDec = 7 + rand.nextInt(12);
+            String lengthBin = Integer.toBinaryString(lengthDec);
+            
+            if(lengthBin.length()<5)
+                vec[0] = 0;
+            
+            //save to 
+            for(int i = 0; i<lengthBin.length(); i++)     
+                vec[i+5-lengthBin.length()] = Integer.parseInt(Character.toString(lengthBin.charAt(i)));
+            
+            //rand fraction (length) 
+            for(int i = 0; i<3; i++)
+                vec[i+5] = rand.nextInt(2);
+        
+            //rand width (Int part): 2-3
+            int widthDec = 2 + rand.nextInt(2);
+            String widthBin = Integer.toBinaryString(widthDec);
+            
+            //save length to array (bin)        
+            for(int i = 0; i<widthBin.length(); i++) 
+                vec[i+8] = Integer.parseInt(Character.toString(widthBin.charAt(i)));
+             //rand fraction part od length 
+            for(int i = 0; i<3; i++)
+                vec[i+10] = rand.nextInt(2);
+            
+            
+            //rand sitplaces
+            int sitMax = (int) (lengthDec*widthDec/(0.7*0.5)-1.5*widthDec-lengthDec*0.5-3*0.24);
+            int sitDec = rand.nextInt(sitMax);
+            String sitBin = Integer.toBinaryString(sitDec);
+            
+            //if num of sit places < 8, fill with 0
+            for(int i = 0; i<(8-sitBin.length()); i++)
+                vec[13+i] = 0;
+                    
+            for(int i = 0; i<sitBin.length(); i++)
+                vec[i+21-sitBin.length()] = Integer.parseInt(Character.toString(sitBin.charAt(i)));
+            
+            
+            return vec;
         }
         
 }
