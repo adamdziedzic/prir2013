8d9558d9bfdf2d665e63a93ac08fa226b6947934
Adam Dziedzic <adamtomaszdziedzic@gmail.com>
2013-06-05 22:24:47 +0200

    server interface changes

--- genethicPRIR/src/genethicprir/PopulationServerInterface.java
+++ genethicPRIR/src/genethicprir/PopulationServerInterface.java
@@ -12,4 +12,7 @@ import java.rmi.RemoteException;
  */
 public interface PopulationServerInterface {
    public SendBundle computeAdaptation(Individual ind) throws RemoteException;
+	public Population getNextPopulation( Population oldPopulation ) throws RemoteException;
+	public Population getFirstPopulation( int numberOfPopulations ) throws RemoteException;
+	public Population getFirstPopulation( int numberOfPopulations, int populationSize) throws RemoteException;
 }
